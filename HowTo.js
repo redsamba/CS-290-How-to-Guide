24e1c232abc0963b0b597370dd065cae0a080823
redsamba <willtimo@oregonstate.edu>
2016-03-07 18:00:51 -0800

    Update HowTo.js

--- HowTo.js
+++ HowTo.js
@@ -14,3 +14,7 @@ app.set('port', 3000);
 app.get('/',function(req,res){
   res.render('howtomain.handlebars') //We can omit the .handlebars extension as we do below
 });
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});

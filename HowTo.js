84145daf5db8441abc0f7063d79d2a91882cac19
redsamba <willtimo@oregonstate.edu>
2016-03-09 21:37:27 -0800

    Update HowTo.js

--- HowTo.js
+++ HowTo.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var bodyParser = require('body-parser');
-//var igdb = require('igdb-api-node');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());

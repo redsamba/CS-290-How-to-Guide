39b9ee5c5589c3e585373234706a3f640cce88a5
redsamba <willtimo@oregonstate.edu>
2016-03-11 15:30:42 -0800

    Update HowTo.js

--- HowTo.js
+++ HowTo.js
@@ -25,7 +25,7 @@ var https = require('https');
 
 var options = {
   host: 'www.igdb.com',
-  path: '/api/v1/games/4873',
+  path: '/api/v1/games/2761',
   port: '443',
   headers: {
     'Accept': 'application/json',
